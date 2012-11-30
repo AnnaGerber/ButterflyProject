e5c1631912479d5032c8c3d65e1af5ea58a29ec0
Anna Gerber <anna.m.gerber@gmail.com>
2012-11-30 11:30:31 +1000

    Added web content from butterflyproject.info

---
+++ www/echojson.php
@@ -0,0 +1,21 @@
+<?php
+/*
+// create a new cURL resource
+$ch = curl_init();
+
+// set URL and other appropriate options
+curl_setopt($ch, CURLOPT_URL, $_GET["url"]);
+curl_setopt($ch, CURLOPT_HEADER, array(
+   'X-ApiKey: yourkeyhere'
+));
+
+// grab URL and pass it to the browser
+curl_exec($ch);
+
+// close cURL resource, and free up system resources
+curl_close($ch);
+*/
+
+echo exec('curl --request GET --header "X-ApiKey: yourkeyhere" '.$_GET["url"]);
+
+?>
